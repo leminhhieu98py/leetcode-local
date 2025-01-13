aa2f8cb3936d6f14157b31daef6deabd4066b8fa
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-13 22:52:25 +0700

    feat: challenge areThereDuplicates using 2 pointers

--- tutorials/2-pointers/areThereDuplicates.js
+++ tutorials/2-pointers/areThereDuplicates.js
@@ -1,8 +1,18 @@
 function areThereDuplicates(...args) {
   // good luck. (supply any arguments you deem necessary.)
-  console.log(args.sort((a, b) => a - b));
+  const stringArgs = args.map((arg) => arg?.toString());
+  const sortStringArgs = stringArgs.sort();
+
+  console.log('sortStringArgs', sortStringArgs)
+
+  for (let i = 0; i < sortStringArgs.length - 1; i++) {
+    if (sortStringArgs[i] === sortStringArgs[i + 1]) return true;
+  }
+
+  return false;
 }
 
-areThereDuplicates(1, 3, 2); // false
-areThereDuplicates(1, 2, 2); // true
-areThereDuplicates('a', 'b', 'c', 'a'); // true
+console.log(areThereDuplicates(1, 3, 2, 5, 4, 7, 9, 13, 4, 17, 9)); // true
+console.log(areThereDuplicates(1, 3, 2, 5, 4, 7)); // false
+console.log(areThereDuplicates(1, 2, 2)); // true
+console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
