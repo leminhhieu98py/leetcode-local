0816a434c5a78d2a85268581179d073155abc176
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-23 23:36:39 +0700

    feat: fibonaci NOT using recursive

--- tutorials/recursive/fibonaci.js
+++ tutorials/recursive/fibonaci.js
@@ -4,6 +4,18 @@ function fibonaci(num) {
   return fibonaci(num - 1) + fibonaci(num - 2);
 }
 
+// function fibonaci(num) {
+//   let sum = 0;
+//   let prevSum = 1;
+
+//   for (let i = 1; i <= num; i++) {
+//     sum += prevSum;
+//     prevSum = sum - prevSum;
+//   }
+
+//   return sum;
+// }
+
 console.log(fibonaci(10)); // 55
 console.log(fibonaci(28)); // 317811
 console.log(fibonaci(35)); // 9227465
