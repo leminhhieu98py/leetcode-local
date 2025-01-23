d1ed8ead58e6fcafa53df997f508073514fbd5e2
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-23 21:40:01 +0700

    feat: collectOddValues using recursive - tutorial

--- tutorials/recursive/collectOddValues.js
+++ tutorials/recursive/collectOddValues.js
@@ -14,5 +14,22 @@ function collectOddValues(arr, index = 0) {
   return collectOddValues(arr, index);
 }
 
+// tutorial solution
+function collectOddValues(arr) {
+  const result = [];
+
+  function helper(helperArr) {
+    if (helperArr.length === 0) return;
+
+    if (helperArr[0] % 2 === 1) result.push(helperArr[0]);
+
+    return helper(helperArr.slice(1));
+  }
+
+  helper(arr);
+
+  return result;
+}
+
 console.log(collectOddValues([1, 2, 3]));
 console.log(collectOddValues([1, 2, 3, 4, 5, 4, 2, 3, 5, 1, 2, 1, 1, 1, 2]));
