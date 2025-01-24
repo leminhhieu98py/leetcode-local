7df76ac315bda1cdacdb21fa9d900dc85cddf74f
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-25 00:06:09 +0700

    refactor: isPalindrome using recursive - tutorial solution

--- tutorials/recursive/isPalindrome.js
+++ tutorials/recursive/isPalindrome.js
@@ -1,14 +1,24 @@
+// Own solution
+// function isPalindrome(str) {
+//   function reverse(str) {
+//     const strLength = str.length;
+//     if (strLength <= 1) return str;
+
+//     return str.slice(-1) + reverse(str.slice(0, strLength - 1));
+//   }
+
+//   const result = reverse(str) === str;
+
+//   return result;
+// }
+
+// Tutorial solution
 function isPalindrome(str) {
-  function reverse(str) {
-    const strLength = str.length;
-    if (strLength <= 1) return str;
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
 
-    return str.slice(-1) + reverse(str.slice(0, strLength - 1));
-  }
-
-  const result = reverse(str) === str;
-
-  return result;
+  return false;
 }
 
 console.log(isPalindrome('awesome')); // false
