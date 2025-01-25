181a35e6b87bda17e3c08c0b444c81a9a53e84a1
leminhhieu98py <leminhhieu98py@gmail.com>
2025-01-25 14:50:14 +0700

    refactor: flatten using recursive

--- tutorials/recursive/flatten.js
+++ tutorials/recursive/flatten.js
@@ -1,20 +1,38 @@
+// Own solution
+// function flatten(arr) {
+//   if (!arr.length) return;
+
+//   const firstElement = arr[0];
+
+//   const remainArr = flatten(arr.slice(1));
+
+//   if (remainArr?.length) {
+//     return Array.isArray(firstElement)
+//       ? flatten(firstElement).concat(remainArr)
+//       : [firstElement].concat(remainArr);
+//   }
+
+//   return Array.isArray(firstElement) ? flatten(firstElement) : [firstElement];
+// }
+
+// tutorial solution
 function flatten(arr) {
-  if (!arr.length) return;
+  let newArr = [];
 
-  const firstElement = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
 
-  const remainArr = flatten(arr.slice(1));
-
-  if (remainArr?.length) {
-    return Array.isArray(firstElement)
-      ? flatten(firstElement).concat(remainArr)
-      : [firstElement].concat(remainArr);
+    if (Array.isArray(element)) {
+      newArr = newArr.concat(flatten(element));
+    } else {
+      newArr.push(element);
+    }
   }
 
-  return Array.isArray(firstElement) ? flatten(firstElement) : [firstElement];
+  return newArr;
 }
 
 console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
 console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
 console.log(flatten([[1], [2], [3]])); // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
