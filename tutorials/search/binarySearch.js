10c0fc8ec862e4cbe27d6ed8910df366cc422d2d
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-05 17:31:34 +0700

    feat: challenge binarySearch with guide

--- tutorials/search/binarySearch.js
+++ tutorials/search/binarySearch.js
@@ -9,28 +9,21 @@ function binarySearch(numArr, num) {
     return -1;
   }
 
-  let left = 0;
-  let right = arrLength - 1;
+  let start = 0;
+  let end = arrLength - 1;
+  let middle = Math.floor((start + end) / 2);
 
-  while (left < right) {
-    const middle = Math.floor((left + right) / 2);
-    const value = numArr[middle];
-
-    if (num === value) {
-      return middle;
-    } else if (num > value) {
-      left = middle;
+  while (numArr[middle] !== num && start <= end) {
+    if (num > numArr[middle]) {
+      start = middle + 1;
     } else {
-      right = middle;
+      end = middle - 1;
     }
 
-    if (left === right - 1) {
-      if (numArr[right] === num) return right;
-      return -1;
-    }
+    middle = Math.floor((start + end) / 2);
   }
 
-  return -1;
+  return numArr[middle] === num ? middle : -1;
 }
 
 console.log(binarySearch([1], 1)); // 0
