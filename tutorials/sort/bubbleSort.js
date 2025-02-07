2b1ece072a085cb8929235d0ab893193d1f38521
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-07 19:18:38 +0700

    feat: challenge bubble search optimization

--- tutorials/sort/bubbleSort.js
+++ tutorials/sort/bubbleSort.js
@@ -4,16 +4,23 @@ const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1
 const bubbleSort = (arr) => {
   if (arr.length <= 1) return arr;
 
+  let noSwap = true;
+
   for (let i = 0; i < arr.length - 1; i++) {
+    noSwap = true;
+
     for (let j = 0; j < arr.length - 1 - i; j++) {
       const currentValue = arr[j];
       const nextValue = arr[j + 1];
 
       // TODO: add a comparator later
       if (currentValue > nextValue) {
+        noSwap = false;
         swap(arr, j, j + 1);
       }
     }
+
+    if (noSwap) break;
   }
 
   return arr;
