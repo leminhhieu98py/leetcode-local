62250d9f41dfa5afa889a5005aa855e15fdd3084
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-08 09:44:34 +0700

    feat: challenge insertionSort with optional comparator function

--- tutorials/sort/insetionSort.js
+++ tutorials/sort/insetionSort.js
@@ -1,11 +1,19 @@
-const insetionSort = (arr = []) => {
+const insertionSort = (arr, comparator) => {
   if (arr.length <= 1) return arr;
 
   for (let i = 1; i < arr.length; i++) {
     const currentValue = arr[i];
     let j = i - 1;
 
-    while (j >= 0 && arr[j] > currentValue) {
+    const condition = (j) => {
+      if (typeof comparator === 'function') {
+        return comparator(arr[j], currentValue) > 0;
+      }
+
+      return arr[j] > currentValue;
+    };
+
+    while (j >= 0 && condition(j)) {
       arr[j + 1] = arr[j];
       j--;
     }
@@ -16,4 +24,17 @@ const insetionSort = (arr = []) => {
   return arr;
 };
 
-console.log(insetionSort([1, 3, 5, 2, 0, 2, 4, 5]));
+console.log(insertionSort([1, 3, 5, 2, 0, 2, 4, 5]));
+
+var kitties = ['LilBub', 'Garfield', 'Heathcliff', 'Blue', 'Grumpy'];
+
+function strComp(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log(insertionSort(kitties, strComp)); // ["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"]
