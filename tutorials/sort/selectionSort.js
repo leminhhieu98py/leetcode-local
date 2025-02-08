268cee7b9abf093aec1cf169bb5d43bb0b8243a2
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-08 08:22:23 +0700

    feat: challenge selectionSort with optional comparator function

--- tutorials/sort/selectionSort.js
+++ tutorials/sort/selectionSort.js
@@ -1,13 +1,19 @@
 const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
 
-const selectionSort = (arr) => {
+const selectionSort = (arr, comparator) => {
   if (arr.length <= 1) return arr;
 
   for (let i = 0; i < arr.length - 1; i++) {
     let minIdx = i;
 
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIdx]) minIdx = j;
+      if (typeof comparator === 'function') {
+        if (comparator(arr[j], arr[minIdx]) < 0) {
+          minIdx = j;
+        }
+      } else if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
 
       if (j === arr.length - 1 && minIdx !== i) {
         swap(arr, minIdx, i);
@@ -19,3 +25,15 @@ const selectionSort = (arr) => {
 };
 
 console.log(selectionSort([1, 3, 2, 4, 5, 1]));
+
+var kitties = ['LilBub', 'Garfield', 'Heathcliff', 'Blue', 'Grumpy'];
+function strComp(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+  return 0;
+}
+
+console.log(selectionSort(kitties, strComp)); // ["Blue", "Garfield", "Grumpy", "Heathcliff", "LilBub"]
