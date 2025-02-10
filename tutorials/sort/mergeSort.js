33506134268504fea2bfb27fde9c3169abaed51d
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-10 19:12:06 +0700

    feat: challenge mergeSort - part 2

--- tutorials/sort/mergeSort.js
+++ tutorials/sort/mergeSort.js
@@ -4,8 +4,18 @@ function merge(arr1, arr2) {
 
   const resultArr = [];
 
-  while (i < arr1.length && j < arr2.length) {
-    console.log({ i, j });
+  while (i < arr1.length || j < arr2.length) {
+    if (i === arr1.length) {
+      resultArr.push(arr2[j]);
+      j++;
+      continue;
+    }
+
+    if (j === arr2.length) {
+      resultArr.push(arr1[i]);
+      i++;
+      continue;
+    }
 
     if (arr1[i] <= arr2[j]) {
       resultArr.push(arr1[i]);
@@ -16,7 +26,6 @@ function merge(arr1, arr2) {
     }
   }
 
-  console.log(resultArr);
   return resultArr;
 }
 
@@ -25,3 +34,4 @@ function mergeSort(arr) {
 }
 
 merge([2, 4, 5, 8, 9, 9, 10], [1, 2, 2, 7]);
+merge([], [1, 2, 2, 7]);
