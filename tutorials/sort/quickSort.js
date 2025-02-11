900d47f4fba24ae3991e738224a20b7a269a9d5a
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-11 23:07:30 +0700

    feat: challenge quickSort - tutorial solution

--- tutorials/sort/quickSort.js
+++ tutorials/sort/quickSort.js
@@ -16,6 +16,40 @@ function quickSort(array = []) {
   return quickSort(leftArr).concat(array[0], quickSort(rightArr));
 }
 
+// Tutorial solution
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
 console.log(quickSort([4, 7, 2, 2, 1, 3, 6])); // [1, 2, 2, 3, 4, 6, 7]
 console.log(quickSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
 console.log(quickSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
