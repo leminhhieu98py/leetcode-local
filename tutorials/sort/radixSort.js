6f116146930bfa7f2b404902dc78b6748ae15f25
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-13 22:52:48 +0700

    feat: challenge radixSort - mostDigit helper

--- tutorials/sort/radixSort.js
+++ tutorials/sort/radixSort.js
@@ -3,9 +3,21 @@ const getDigit = (num, indexFromRight) =>
 
 const digitCount = (num) => Math.floor(Math.log10(Math.abs(num))) + 1;
 
+const mostDigit = (numArr = []) => {
+  let max = 1;
+
+  for (let i = 0; i < numArr.length; i++) {
+    max = Math.max(digitCount(numArr[i]), max);
+  }
+
+  return max;
+};
+
 console.log(getDigit(123450, 0)); // 0
 console.log(getDigit(123450, 1)); // 5
 console.log(getDigit(123450, 2)); // 4
 console.log(getDigit(123450, 3)); // 3
 console.log(getDigit(123450, 4)); // 2
 console.log(getDigit(123450, 9)); // 1
+
+console.log(mostDigit([1234, 2, 5])); // 4
