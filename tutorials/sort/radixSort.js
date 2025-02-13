2e3964b0413ada3e2a832c8c267576c830fe130a
leminhhieu98py <leminhhieu98py@gmail.com>
2025-02-13 22:47:52 +0700

    feat: challenge radixSort - digitCount helper

--- tutorials/sort/radixSort.js
+++ tutorials/sort/radixSort.js
@@ -1,6 +1,8 @@
 const getDigit = (num, indexFromRight) =>
   Math.floor((Math.abs(num) / Math.pow(10, indexFromRight)) % 10);
 
+const digitCount = (num) => Math.floor(Math.log10(Math.abs(num))) + 1;
+
 console.log(getDigit(123450, 0)); // 0
 console.log(getDigit(123450, 1)); // 5
 console.log(getDigit(123450, 2)); // 4
