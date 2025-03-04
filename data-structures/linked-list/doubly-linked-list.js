43959589195f1c9cbd2742c6d308cab99ecaecf1
leminhhieu98py <leminhhieu98py@gmail.com>
2025-03-04 07:29:23 +0700

    set method for the doubly linked list

--- data-structures/linked-list/doubly-linked-list.js
+++ data-structures/linked-list/doubly-linked-list.js
@@ -107,6 +107,18 @@ class DoublyLinkedList {
 
     return current;
   }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+
+    if (foundNode) {
+      foundNode.val = val;
+
+      return true;
+    }
+
+    return false;
+  }
 }
 
 const doublyLinkedList = new DoublyLinkedList();
