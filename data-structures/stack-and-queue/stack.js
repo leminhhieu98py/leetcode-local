ea1f2f7d1725efed3bbd7065efa3fa4fd826ad0b
leminhhieu98py <leminhhieu98py@gmail.com>
2025-03-08 21:53:55 +0700

    method push for the stack

--- data-structures/stack-and-queue/stack.js
+++ data-structures/stack-and-queue/stack.js
@@ -2,7 +2,7 @@
 
 class Node {
   constructor(val) {
-    this.val = val;
+    this.value = val;
     this.next = null;
   }
 }
@@ -12,5 +12,21 @@ class Stack {
     this.first = null;
     this.last = null;
     this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    newNode.next = this.first;
+
+    this.first = newNode;
+
+    if (!this.last) {
+      this.last = newNode;
+    }
+
+    this.size++;
+
+    return this;
   }
 }
