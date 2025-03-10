9367290140f373bc52cd5ca5b6d74051c4d12178
leminhhieu98py <leminhhieu98py@gmail.com>
2025-03-10 19:08:39 +0700

    initial for the Queue

---
+++ data-structures/stack-and-queue/queue.js
@@ -0,0 +1,16 @@
+// First in first out (FIFO)
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
