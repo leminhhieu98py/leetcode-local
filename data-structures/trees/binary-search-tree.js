cc66d4e71c02e7274c0f006c9770761090d799d9
leminhhieu98py <leminhhieu98py@gmail.com>
2025-03-13 00:04:09 +0700

    feat: implement insert method for the Binary Search Tree

--- data-structures/trees/binary-search-tree.js
+++ data-structures/trees/binary-search-tree.js
@@ -10,4 +10,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    while (current !== null) {
+      if (current.value === value) return undefined;
+
+      if (current.left === null && value < current.value) {
+        current.left = newNode;
+        return this;
+      }
+
+      if (current.right === null && value > current.value) {
+        current.right = newNode;
+        return this;
+      }
+
+      current = value > current.value ? current.right : current.left;
+    }
+
+    return this;
+  }
 }
